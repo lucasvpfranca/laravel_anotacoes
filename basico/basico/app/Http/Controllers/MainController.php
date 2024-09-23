c777b53a9d23080dd3ac9fc9e7be7b01f24132a6
Lucas França <lucasvpfranca@gmail.com>
2024-09-23 18:44:28 -0300

    Finalizado Login e Pagina Notes

--- basico/basico/app/Http/Controllers/MainController.php
+++ basico/basico/app/Http/Controllers/MainController.php
@@ -2,26 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Operations;
 use App\User;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
     public function index() {
 
         $id = session('user.id');
-        // $user = User::find($id)->toArray();
-        $notes =  User::find($id)->notes()->get()->toArray();
+            // $user = User::find($id)->toArray();
+            $notes =  User::find($id)->notes()->get()->toArray();
 
-        // echo "<pre>";
-        // print_r($user);
-        // print_r($notes);
-        // die();
-        return view('home', ['notes' => $notes]);
+            // echo "<pre>";
+            // print_r($user);
+            // print_r($notes);
+            // die();
+            return view('home', ['notes' => $notes]);
     }
+
+
 
     public function newNote() {
-        echo "Criando NOTE";
+
+        return view('new_note');
     }
+    public function newNoteSubmit(Request $r) {
+        echo "Criado nota";
+    }
+
+    public function editNote($id) {
+
+        $id = Operations::decryptId($id);
+
+
+    }
+
+    public function deleteNote($id) {
+
+        $id = Operations::decryptId($id);
+        // $id = $this->decryptId($id);
+
+
+    }
+
+
 
 }
--- basico/basico/routes/web.php
+++ basico/basico/routes/web.php
@@ -32,7 +32,10 @@ Route::post('loginSubmit', [AuthController::class, 'loginSubmit']);
 Route::middleware([CheckIsLogged::class])->group(function () {
 
     Route::get('/', [MainController::class, 'index'])->name('home');
-    Route::get('/newNote', [MainController::class, 'newNote'])->name('newNote');;
-    Route::get("logout", [AuthController::class, 'logout'])->name('logout');;
+    Route::get('/newNote', [MainController::class, 'newNote'])->name('newNote');
+    Route::post('/newNoteSubmit', [MainController::class, 'newNoteSubmit'])->name('newNoteSubmit');
+    Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');
+    Route::get('/deleteNote/{id}', [MainController::class, 'deleteNote'])->name('delete');
+    Route::get("logout", [AuthController::class, 'logout'])->name('logout');
 });
 
